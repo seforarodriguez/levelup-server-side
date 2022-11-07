db8c169258fb10a12af6efa0442101b360f1be79
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-11-07 15:22:18 -0500

    updates settings to take secretkey out, updated url paths to have the register and login view, added the gametype vew

--- levelup/urls.py
+++ levelup/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+from levelupapi.views import GameTypeView
 from levelupapi.views import register_user, login_user
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
